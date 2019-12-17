49351c5e3dc11db578de1376500af2e17a590050
Alec Hoyland <alec.hoyland@posteo.net>
2019-12-17 16:49:31 -0500

    Add new functions

---
+++ exgauss_kernel.m
@@ -0,0 +1,15 @@
+% create an exgaussian kernel
+
+function k = exgauss_kernel(bandwidth, params, notch)
+
+  k = exgauss_pdf(bandwidth, mu, sigma, tau);
+
+  if ~exist('notch', 'var') || isempty(notch)
+    % do nothing
+  else
+    % notch the kernel at t = 0
+    k(1) = 0;
+    k = k / sum(k);
+  end
+
+end % function
