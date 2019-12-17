aaf7d9359f60d250e91bffa9d1ffd72fc46bd14f
Alec Hoyland <alec.hoyland@posteo.net>
2019-12-17 18:13:19 -0500

    Include an alpha parameter

--- exgauss_kernel.m
+++ exgauss_kernel.m
@@ -2,14 +2,14 @@
 
 function k = exgauss_kernel(bandwidth, params, notch)
 
-  k = exgauss_pdf(bandwidth, mu, sigma, tau);
+  k = params(1) * exgauss_pdf(bandwidth, params(2), params(3), params(4));
 
   if ~exist('notch', 'var') || ~notch
     % do nothing
   else
     % notch the kernel at t = 0
     k(1) = 0;
-    k = k / sum(k);
+    k = params(1) * k / sum(k);
   end
 
 end % function
