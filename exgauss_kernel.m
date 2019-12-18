2ff5eecfff2c12a379f8a82de3f611cfa4767de8
Alec Hoyland <alec.hoyland@posteo.net>
2019-12-18 12:19:36 -0500

    Fix normalization

--- exgauss_kernel.m
+++ exgauss_kernel.m
@@ -10,14 +10,17 @@
 
 function k = exgauss_kernel(x, params, notch)
 
-  k = params(1) * exgauss_pdf(x, params(2), params(3), params(4));
+  % compute the (unnormalized) pdf
+  k = exgauss_pdf(x, params(2), params(3), params(4));
 
   if ~exist('notch', 'var') || ~notch
     % do nothing
   else
     % notch the kernel at t = 0
     k(1) = 0;
-    k = params(1) * k / sum(k);
   end
+
+    % normalize and scale by alpha
+    k = params(1) * k / sum(k);
 
 end % function
