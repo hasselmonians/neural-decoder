92c7a95d2d21b695947fa08bb1fddb794a1448f0
Alec Hoyland <alec.hoyland@posteo.net>
2020-01-07 13:06:34 -0500

    Add function to produce nldat object

---
+++ @NeuralDecoder/nldat.m
@@ -0,0 +1,18 @@
+function iodata = nldat(self, u, z)
+
+  % produces an nldat object (see NLID-matlab-toolbox)
+  % from the raw data stored in the NeuralDecoder object
+
+  % Arguments:
+  %   self: the NeuralDecoder object
+  %   u: the signal (viz. running speed, head direction) as a vector of doubles
+  %   z: the response (viz. firing rate) as a vector of doubles
+
+  iodata = nldat([u,z], ...
+    'domainincr', self.Fs, ...
+    'domainvalues', self.timestamps, ...
+    'domainname', 'Time',...
+    'chanunits', { 'a.u.', 'Hz'}, ...
+    'channames', { 'extrinsic signal', 'firing rate'};
+
+end % function
