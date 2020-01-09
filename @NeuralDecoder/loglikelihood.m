d94d64fba4bb82bd120c6bbc55a3c16c8b883879
Alec Hoyland <alec.hoyland@posteo.net>
2020-01-09 14:45:56 -0500

    Rename function arguments, clean up documentation

--- @NeuralDecoder/loglikelihood.m
+++ @NeuralDecoder/loglikelihood.m
@@ -1,24 +1,25 @@
 % loglikelihood
 % computes the log likelihood of a non-homogeneous Poisson process
-% where transformed_signal is the Poisson rate
+% where rate is the Poisson rate
 % and the Poisson process produces a given spike train
 %
 %% Arguments:
 %   self: a NeuralDecoder object
-%   transformed_signal: an estimate for the underlying firing rate
+%   rate: an estimate for the underlying firing rate
 %     this is a vector probably computed by kconv
 %
-%% Outputs: the loglikelihood as a scalar
+%% Outputs:
+%   L: the loglikelihood as a scalar
 %
 % See Also: NeuralDecoder.kernelCore, NeuralDecoder.cvKernel
 
-function L = loglikelihood(self, transformed_signal)
+function L = loglikelihood(self, rate)
 
   dt = 1 / self.Fs;
 
   L = sum( ...
-    - transformed_signal * dt ...
-    + self.spikeTrain .* log(transformed_signal) ...
+    - rate * dt ...
+    + self.spikeTrain .* log(rate) ...
     + self.spikeTrain * log(dt) ...
     - log(factorial(self.spikeTrain)));
 
