9fec4cbc3a1711394748c1b299db8e58719c4a75
Alec Hoyland <alec.hoyland@posteo.net>
2020-01-10 15:10:20 -0500

    Compute the kernel support from a given kernel or default to bandwidth

--- @NeuralDecoder/getKernelSupport.m
+++ @NeuralDecoder/getKernelSupport.m
@@ -1,8 +1,12 @@
-function kernel_support = getKernelSupport(self)
+function kernel_support = getKernelSupport(self, kernel)
 
   % computes the time-axis for the kernel
   % from the bandwidth and sampling frequency
 
-  kernel_support = colon(0, 1 / self.Fs, self.bandwidth);
+  if nargin < 2
+    kernel_support = colon(0, 1 / self.Fs, self.bandwidth);
+  else
+    kernel_support = 1 / self.Fs * colon(0, 1, kernel);
+  end
 
 end % function
