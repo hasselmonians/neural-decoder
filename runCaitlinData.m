837918114c859678711d5f046f1d1b22ab604ee8
Alec Hoyland <alec.hoyland@posteo.net>
2020-01-14 11:40:05 -0500

    Add helper script to load jobs onto the cluster

---
+++ runCaitlinData.m
@@ -0,0 +1,25 @@
+r = RatCatcher;
+
+r.expID = 'Caitlin';
+r.remotepath = '/projectnb/hasselmogrp/ahoyland/neural-decoder/cluster/';
+r.localpath = '/mnt/hasselmogrp/ahoyland/neural-decoder/cluster/';
+r.protocol = 'NeuralDecoder';
+r.project = 'hasselmogrp';
+r.verbose = true;
+
+return
+
+% batch files
+r = r.batchify;
+
+% NOTE: run the 'qsub' command on the cluster now (see output in MATLAB command prompt)
+
+return
+
+% NOTE: once the cluster finishes, run the following commands
+
+% gather files
+r = r.validate;
+dataTable = r.gather;
+save('data/Caitlin-NeuralDecoder.mat', 'dataTable', 'r')
+% dataTable = r.stitch(dataTable);
