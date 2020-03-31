3f51d22cc31f1e5775bab7dbcff1e4cf34d77072
Alec Hoyland <ahoyland@brandeis.edu>
2020-03-31 15:46:41 -0400

    Add adaptive sampling

--- tests/test_adaptive_kernel.m
+++ tests/test_adaptive_kernel.m
@@ -10,30 +10,17 @@ options = generateSampleData();
 
 %% Sample the parameters
 
-% useful variables
-nSims = 100;
-rand_params = randi(100, 100, 4);
-w = neurodec.getKernelSupport();
+a = adaptive();
+a.MaxIter = 20;
+a.Lower = [0, 0];
+a.Upper = [100, 100];
+a.SampleFcn = @(params) neurodec.objective_function(signal, params);
 
-% only examine two parameters (ignore alpha and mu)
-rand_params(:, 1) = options.Params(1);
-rand_params(:, 2) = options.Params(2);
-
-% output variables
-params = NaN(nSims, 4);
-objective = NaN(nSims, 1);
-logL = NaN(nSims, 1);
-
-for ii = 1:nSims
-    corelib.textbar(ii, nSims)
-  [objective(ii), logL(ii)] = objective_function(neurodec, signal, rand_params(ii, :));
-end
-
-%% Output
-
-alpha   = rand_params(:, 1);
-mu      = rand_params(:, 2);
-sigma   = rand_params(:, 3);
-tau     = rand_params(:, 4);
-
-data_table = table(alpha, mu, sigma, tau, logL, objective);
+% %% Output
+%
+% alpha   = rand_params(:, 1);
+% mu      = rand_params(:, 2);
+% sigma   = rand_params(:, 3);
+% tau     = rand_params(:, 4);
+%
+% data_table = table(alpha, mu, sigma, tau, logL, objective);
