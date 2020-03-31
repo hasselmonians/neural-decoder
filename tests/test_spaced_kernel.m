958c88352a08946532c13832599d8a42900a6bbb
Alec Hoyland <ahoyland@brandeis.edu>
2020-03-31 16:11:48 -0400

    Refactor to use log-spaced measures

--- tests/test_spaced_kernel.m
+++ tests/test_spaced_kernel.m
@@ -1,4 +1,4 @@
-%% test_rand_kernel.m
+%% test_spaced_kernel.m
 % Use a test signal and known kernel parameters.
 % Sample many parameters and compute the log likelihood
 % as defined by the cost function.
@@ -11,13 +11,20 @@ options = generateSampleData();
 %% Sample the parameters
 
 % useful variables
-nSims = 100;
-rand_params = randi(100, 100, 4);
+nSamples = 5;
+nSims = nSamples * nSamples;
+these_params = NaN(nSims, 4);
 w = neurodec.getKernelSupport();
 
 % only examine two parameters (ignore alpha and mu)
-rand_params(:, 1) = options.Params(1);
-rand_params(:, 2) = options.Params(2);
+these_params(:, 1) = options.Params(1);
+these_params(:, 2) = options.Params(2);
+
+% tile a log-spaced domain in terms of Params(3) and Params(4)
+exps = linspace(-2, 2, nSamples);
+these_combinations = nchoosek(nSamples);
+these_params(:, 3) = options.Params(3) ^ exps(these_combinations(:, 1));
+these_params(:, 3) = options.Params(4) ^ exps(these_combinations(:, 2));
 
 % output variables
 params = NaN(nSims, 4);
@@ -26,14 +33,14 @@ logL = NaN(nSims, 1);
 
 for ii = 1:nSims
     corelib.textbar(ii, nSims)
-  [objective(ii), logL(ii)] = objective_function(neurodec, signal, rand_params(ii, :));
+  [objective(ii), logL(ii)] = objective_function(neurodec, signal, these_params(ii, :));
 end
 
 %% Output
 
-alpha   = rand_params(:, 1);
-mu      = rand_params(:, 2);
-sigma   = rand_params(:, 3);
-tau     = rand_params(:, 4);
+alpha   = these_params(:, 1);
+mu      = these_params(:, 2);
+sigma   = these_params(:, 3);
+tau     = these_params(:, 4);
 
 data_table = table(alpha, mu, sigma, tau, logL, objective);
