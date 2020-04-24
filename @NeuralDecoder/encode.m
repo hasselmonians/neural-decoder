d5bde458911c96dd0c6670ba43b1c77fe2b84e7e
Alec Hoyland <alec.hoyland@posteo.net>
2020-04-24 10:46:17 -0400

    Split encode into two functions, a package function and a class method

--- @NeuralDecoder/encode.m
+++ @NeuralDecoder/encode.m
@@ -1,18 +1,19 @@
-% generate Poisson-distributed data based on a non-homogeneous rate parameter
-% generate the non-homogeneous rate parameter by convolving a known signal
-% with an exponentially-modified Gaussian kernel
-%
-% Arguments:
-%   % raw_signal: a time-varying rate used to generate the Poisson-distributed spike train
-%   % kernel: the vector to convolve the raw signal with
-%
-% Outputs:
-%   transformed_signal: the rate used as a time-varying parameter of the Poisson process
 
-function [transformed_signal] = encode(raw_signal, kernel)
+function [transformed_signal] = encode(self, raw_signal)
+
+  %% Description
+  %   generate Poisson-distributed data based on a non-homogeneous rate parameter
+  %   generate the non-homogeneous rate parameter by convolving a known signal
+  %   with an exponentially-modified Gaussian kernel
+  %
+  %% Arguments:
+  %   % raw_signal: a time-varying rate used to generate the Poisson-distributed spike train
+  %
+  %% Outputs:
+  %   transformed_signal: the rate used as a time-varying parameter of the Poisson process
 
   % generate the transformed signal by taking the convolution
-  transformed_signal = conv(raw_signal, kernel, 'full');
+  transformed_signal = conv(raw_signal, self.kernel, 'full');
   % truncate the signal to the same size as the raw signal
   transformed_signal = transformed_signal(1:length(raw_signal));
 
