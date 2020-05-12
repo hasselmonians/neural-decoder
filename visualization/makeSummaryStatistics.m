bcdf08ff039ecd18fc590d1a47096e8d79070029
Alec Hoyland <alec.hoyland@posteo.net>
2020-05-12 12:47:12 -0400

    Add script for aggregrating figures

---
+++ visualization/makeSummaryStatistics.m
@@ -0,0 +1,99 @@
+% show summary statistics of the data table
+% containing the four parameters of the EMG kernel
+% after running the NeuralDecoder protocol on Caitlin's dataset
+
+being_published = true;
+pdflib.header;
+tic;
+
+% load the data
+package_dir = pathlib.strip(mfilename('fullpath'), 2);
+load(fullfile(package_dir, 'data', 'Caitlin-SM-NeuralDecoder.mat'));
+
+%% Introduction
+% Take Caitlin's dataset of rats freely exploring an empty, square arena,
+% and compute the relationship between the firing rate of
+% putatively speed-modulated cells,
+% and the running speed of the animal.
+% We assume that this relationship is given by convolution
+% of the running speed with a four-parameter exponentially-modified
+% gaussian kernel.
+%
+% The four parameters are |alpha|, |mu|, |sigma|, and |tau|.
+% |alpha| is a scaling parameter which accounts for differences
+% in magnitude between the running speed and the firing rate.
+% It is bounded in |[0, 2]|.
+% |mu| is the lag parameter and analogous to the parameter of the same name
+% in the gaussian probability density function.
+% It is bounded in |[0, 40]|.
+% |sigma| is the spread parameter,
+% the correlate of the gaussian standard deviation parameter,
+% and it is bounded in |[0, 5]|.
+% |tau| is the decay parameter, is analogous to the time constant
+% of an exponential probability density function,
+% and is bounded in |[0, 3]|.
+%
+% Caitlin's dataset was separated into putatively speed-modulated,
+% and putatively non-speed-modulated cells
+% by a threshold of |R >= 0.0795| between the running speed signal
+% and the firing rate of the cell as computed by
+% the maximum-likelihood estimate with cross-validation approach
+% using a Hann kernel (Prerau & Eden 2011, Dannenberg et al. 2019).
+
+%% Summary statistics
+
+summary(data_table)
+
+%% Scatter plots with marginal histograms
+
+parameter_names = {'alpha', 'mu', 'sigma', 'tau'};
+parameter_combinations = nchoosek(parameter_names, 2);
+
+for ii = colon(size(parameter_combinations, 1), -1, 1)
+	figure('OuterPosition',[0 0 1600 1600],'PaperUnits','points','PaperSize',[1600 1600]);
+
+	s(ii) = scatterhistogram(data_table, parameter_combinations{ii, 1}, parameter_combinations{ii, 2}, ...
+	  'HistogramDisplayStyle', 'smooth', ...
+	  'LineStyle', '-');
+
+	xlabel(['\' parameter_combinations{ii, 1}])
+	ylabel(['\' parameter_combinations{ii, 2}])
+
+	if being_published
+	  pdflib.snap()
+	  delete(gcf)
+	end
+end
+
+%% Samples
+
+for ii = 1:height(data_table)
+	plotTimeSeries(data_table, ii, ...
+		'PreProcessFcn', @(x) strrep(x, 'projectnb', 'mnt'), ...
+		'Verbosity', false);
+	if being_published
+		pdflib.snap()
+		delete(gcf)
+	end
+end
+
+%% Version Info
+% The file that generated this document is called:
+disp(mfilename)
+
+%%
+% and its md5 hash is:
+disp(hashlib.md5hash(strcat(mfilename, '.m'), 'File'))
+
+%%
+% This file should be in this commit:
+[status,m]=unix('git rev-parse HEAD');
+if ~status
+	disp(m)
+end
+
+t = toc;
+
+%%
+% This file has the following external dependencies:
+pdflib.showDependencyHash(mfilename);
