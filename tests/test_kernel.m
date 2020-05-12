b8f75d6feb9b6b2ecf0b8a196544665d3bff9b17
Alec Hoyland <alec.hoyland@posteo.net>
2020-05-12 13:51:13 -0400

    Add more figures

--- tests/test_kernel.m
+++ tests/test_kernel.m
@@ -20,6 +20,8 @@ nKernels = length(params);
 % tack on the alpha parameter, fixed to unity
 params = [ones(nKernels, 1) params];
 
+%% Compute the kernels
+
 % container for storing kernels
 k = NaN(length(w), nKernels);
 
@@ -30,6 +32,33 @@ for ii = 1:nKernels
 end
 
 %% Plot the kernels
+
+figure; hold on;
+l = colormaps.linspecer(nKernels);
+for ii = 1:nKernels
+    plot(w, k(:, ii), 'Color', l(ii, :));
+end
+
+title('a bunch of kernels')
+xlabel('kernel support (s)')
+ylabel('kernel density')
+figlib.pretty('PlotBuffer', 0.1, 'PlotLineWidth', 1)
+
+%% Show effect of mu with all other parameters constant
+
+nKernels = 11;
+mus = linspace(0, 30, nKernels);
+params = zeros(nKernels, 4);
+params(:, 1) = 1;
+params(:, 3) = 5;
+params(:, 4) = 3;
+params(:, 2) = mus;
+
+% compute the kernels
+for ii = 1:nKernels
+    corelib.textbar(ii, nKernels);
+    k(:, ii) = exgauss_kernel(w, params(ii, :));
+end
 
 figure; hold on;
 l = colormaps.linspecer(nKernels);
