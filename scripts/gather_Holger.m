5bd23ce053cde3faf63b7a29455532d3961ffeb4
Alec Hoyland <alec.hoyland@posteo.net>
2020-05-20 11:02:42 -0400

    Refactor the save path to be more robust

--- scripts/gather_Holger.m
+++ scripts/gather_Holger.m
@@ -4,4 +4,6 @@ r = r.validate;
 data_table = r.gather;
 data_table = r.stitch(data_table);
 
-save(['../data/' r.batchname '.mat'], 'data_table', 'r')
+save_path = fullfile(pathlib.strip(mfilename('fullpath'), 2), 'data', [r.batchname '.mat']);
+disp(['saving data to ' save_path])
+save(save_path, 'data_table', 'r')
